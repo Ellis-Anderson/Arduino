e7246362cf4b69b17dd8c1112bcdd4f46d369280
dattr <dattrnka@gmail.com>
2018-02-18 15:21:15 -0800

    adding spectrum_share to test git functionality

---
+++ Sketches/Radio_Lights/Spectrum_Share/Spectrum_Share/Spectrum_Share.ino
@@ -0,0 +1,143 @@
+#include <Si4703_Breakout.h>
+#include <Wire.h>
+
+int resetPin = 2;
+int SDIO = A4;
+int SCLK = A5;
+
+Si4703_Breakout radio(resetPin, SDIO, SCLK);
+int channel;
+int volume;
+char rdsBuffer[10];
+
+//Declare Spectrum Shield pin connections
+#define STROBE 4
+#define RESET 5
+#define DC_One A0
+#define DC_Two A1 
+
+
+//Define spectrum variables
+int freq_amp;
+int Frequencies_One[7];
+int Frequencies_Two[7]; 
+int i;
+int PrevFreq;
+int l;
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("\n\nSi4703_Breakout Test Sketch");
+  Serial.println("===========================");  
+  Serial.println("a b     Favourite stations");
+  Serial.println("+ -     Volume (max 15)");
+  Serial.println("u d     Seek up / down");
+  Serial.println("r       Listen for RDS Data (15 sec timeout)");
+  Serial.println("Send me a command letter.");
+  
+
+  radio.powerOn();
+    Serial.println("I've come through");
+  radio.setVolume(15);
+  radio.setChannel(1013);
+
+
+
+  //Set spectrum Shield pin configurations
+  pinMode(STROBE, OUTPUT);
+  pinMode(RESET, OUTPUT);
+  pinMode(DC_One, INPUT);
+  pinMode(DC_Two, INPUT);  
+  digitalWrite(STROBE, HIGH);
+  digitalWrite(RESET, HIGH);
+
+  //Initialize Spectrum Analyzers
+  digitalWrite(STROBE, LOW);
+  delay(1);
+  digitalWrite(RESET, HIGH);
+  delay(1);
+  digitalWrite(STROBE, HIGH);
+  delay(1);
+  digitalWrite(STROBE, LOW);
+  delay(1);
+  digitalWrite(RESET, LOW);
+
+}
+
+void loop()
+{
+  if (Serial.available())
+  {
+    char ch = Serial.read();
+    if (ch == 'u') 
+    {
+      channel = radio.seekUp();
+      displayInfo();
+    } 
+    else if (ch == 'd') 
+    {
+      channel = radio.seekDown();
+      displayInfo();
+    } 
+    else if (ch == '+') 
+    {
+      volume ++;
+      if (volume == 16) volume = 15;
+      radio.setVolume(volume);
+      displayInfo();
+    } 
+    else if (ch == '-') 
+    {
+      volume --;
+      if (volume < 0) volume = 0;
+      radio.setVolume(volume);
+      displayInfo();
+    } 
+    else if (ch == 'a')
+    {
+      channel = 1053; // Rock FM
+      radio.setChannel(channel);
+      displayInfo();
+    }
+    else if (ch == 'b')
+    {
+      channel = 974; // BBC R4
+      radio.setChannel(channel);
+      displayInfo();
+    }
+    else if (ch == 'r')
+    {
+      Serial.println("RDS listening");
+      radio.readRDS(rdsBuffer, 15000);
+      Serial.print("RDS heard:");
+      Serial.println(rdsBuffer);      
+    }
+  }
+
+    Read_Frequencies();
+    
+}
+
+void displayInfo()
+{
+   Serial.print("Channel:"); Serial.print(channel); 
+   Serial.print(" Volume:"); Serial.println(volume); 
+}
+
+
+/*************Pull frquencies from Spectrum Shield****************/
+void Read_Frequencies(){
+  //Read frequencies for each band
+  for (freq_amp = 0; freq_amp<7; freq_amp++)
+  {
+    Frequencies_One[freq_amp] = analogRead(DC_One);
+    Frequencies_Two[freq_amp] = analogRead(DC_Two); 
+    digitalWrite(STROBE, HIGH);
+    delay (50);
+    digitalWrite(STROBE, LOW);
+    delay (50);
+  }
+  
+}
